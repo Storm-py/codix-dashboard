66a8f938c3c2cfaaaa7dd426c0a59472fa4e77a9
Storm-py <ranamuneebmay2018@gmail.com>
2024-11-04 17:19:20 +0500

    Add consoles to check the error

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -26,8 +26,11 @@ export default function SignUp() {
         body: JSON.stringify({ name, email, password }),
       });
 
+      console.log("This is the response to the server", response)
+      
       if (response.ok) {
         const data = await response.json();
+        console.log("This is the data to the server", data)
         setSuccess(data.message);
         setTimeout(() => {
           router.push('/signin');
@@ -61,8 +64,6 @@ export default function SignUp() {
                   id="name"
                   name="name"
                   type="text"
-                  autoComplete="name"
-                  required
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -80,8 +81,6 @@ export default function SignUp() {
                   id="email"
                   name="email"
                   type="email"
-                  autoComplete="email"
-                  required
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -99,8 +98,6 @@ export default function SignUp() {
                   id="password"
                   name="password"
                   type="password"
-                  autoComplete="new-password"
-                  required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
